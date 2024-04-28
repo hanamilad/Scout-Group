383ef54f55ca5bdc4522775e010f266b1577088d
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-04-28 15:09:41 +0300

    Update FunApi.jsx

--- app/_axios/FunApi.jsx
+++ app/_axios/FunApi.jsx
@@ -1,11 +1,11 @@
 
 const { default: axiosApi } = require("./MemberApi");
 
-const getallmember=()=>axiosApi.get("/api/members?populate=*&pagination[pageSize]=100")
+const getallmember=()=>axiosApi.get("/api/members?populate=*&pagination[pageSize]=100&sort[0]=id")
 const postUserData=(data)=>axiosApi.post("/api/members",data)
 const deleteuser = (id) => axiosApi.delete(`/api/members/${id}`);
 const updateuser = (id,data) => axiosApi.put(`/api/members/${id}`,{data:data});
-const getGoals =()=>axiosApi.get("/api/goals?pagination[pageSize]=100")
+const getGoals =()=>axiosApi.get("/api/goals?pagination[pageSize]=100&sort[0]=id")
 const deletgoals=(id)=>axiosApi.delete(`/api/goals/${id}`)
 const updategoals=(id,data)=>axiosApi.put(`/api/goals/${id}`,{data:data})
 const addgoals=(data)=>axiosApi.post("/api/goals",data) 
